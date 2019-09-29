ad520e353aa13013c507b4950704bdb72e410d5a
Julia <vardiny>
2019-09-29 16:10:42 +0300

    correct the error

--- 1 homework/1.py
+++ 1 homework/1.py
@@ -1,7 +1,7 @@
 n = float(input())
 s = float(input())
 if n > s:
-    print('first number more than second ')
+    print('first number more than second')
 elif n < s:
     print('second number more than first')
 else:
--- 1 homework/5.py
+++ 1 homework/5.py
@@ -1,5 +1,5 @@
-x=float(input())
-y=float(input())
+x = float(input())
+y = float(input())
 if x > 0 and y > 0:
     print('1/4 on 1')
 elif x < 0 and y > 0:
