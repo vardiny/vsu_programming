99920c89d9ddc69f8b4072d1f8e3ade0c08afd25
Julia <vardiny>
2019-12-09 22:06:33 +0300

    redone 6 task

--- 6 homework/1.3.py
+++ 6 homework/1.3.py
@@ -1,5 +1,5 @@
-size = 13
-storage = [None] * size
+size = 6
+storage = [ [] for _ in range(size)]
  
 def hash(key):
     result = 0
@@ -9,23 +9,47 @@ def hash(key):
  
 def set_value(key, value):
     index = hash(key)
-    while storage[index]:
-        index += 1
-    storage[index] = key, value
+    for x in storage[index]:
+        if x[0] == key:
+            x[1] = value
+            break
+    else:
+        storage[index].append([key, value])
  
 def get_value(key):
     index = hash(key)
-    key2, value = storage[index]
-    while key2 != key:
-        index += 1
-        key2, value = storage[index]
-    return value
+    for i in storage[index]:
+        if key == i[0]:
+            return i[1]
+
+def del_value(key):
+    index = hash(key)
+    for i in storage[index]:
+        if key == i[0]:
+            storage[index].remove(i)
+            break
  
- 
+set_value('abc', 1)
+set_value('abc', 2)
+
+set_value('cba', 4)
+set_value('cba', 10)
+
+set_value('abdc', 1123)
+set_value('abdc', 2123)
+set_value('zxc', 2123)
+
+set_value('XYN', 3)
+set_value('XYN', 11)
 print(storage)
-set_value('abc', 7)
-print(storage) 
-set_value('cba', 42)
-print(storage)
+del_value('XYN')
+
+print(get_value('abc'))
 print(get_value('cba'))
+print(get_value('XYN'))
+
+print(get_value('abdc'))
+print(get_value('abdc'))
+print(get_value('zxc'))
+
 print(storage)
